45fda6e3ab4cb8007ffac5f091aec139f83ff705
Chayan jain <chayantwinkle@gmail.com>
2025-02-11 12:02:04 +0530

    addition of usestate hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Button from './components/botton'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
           {/* <Button name={"chayan"}/> */}
           <Button name={{"name":"chayan"}} onClick={()=>{alert("7")}}/>
-        </a>
       </header>
     </div>
   );
--- src/components/botton.tsx
+++ src/components/botton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useState } from "react";
 
 interface button1 {
     name:{
@@ -8,14 +8,25 @@ interface button1 {
     onClick: () => void
 }
 
+interface book{
+    "name":string,
+    "author":string
+  }
+
 // type button1  ={
 //     name:string
 // }
 
 const Button:React.FC<button1>= (props)=>{
+    // const [value,setValue] = useState<book>({"author":"chayan","name":"book"})
+    const [value,setValue] = useState<string>("b")
 
     return (
-        <><button onClick={props.onClick} >{props.name.name}</button></>
+        <>
+        
+        <h3>{value}</h3>
+        <button onClick={()=>setValue("a")} >{props.name.name}</button>
+        </>
     )
 }
 
